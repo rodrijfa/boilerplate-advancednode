169d080449d05b34c62ba689d60433a45c90762a
rodrijfa <rodrijfa@gmail.com>
2026-02-07 11:14:25 -0400

    Refactor server.js for consistency and clarity

--- server.js
+++ server.js
@@ -6,33 +6,22 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); 
+fccTesting(app);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/views/pug');
 
-app.set("view engine", "pug");
-app.set("views", "./views/pug");
-
-
-  
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port' + PORT);
+  console.log('Listening on port ' + PORT);
 });
+
 
